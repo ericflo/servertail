d8ab63efedd876bfcddc16eff1e0ccfd0483f7a0
Eric Florenzano <floguy@gmail.com>
2010-08-14 12:55:57 -0700

    Rewrote all the javascript

--- media/js/tail.js
+++ media/js/tail.js
@@ -1,7 +1,10 @@
-var cursor = null;
-var waitTime = 0;
-
-function tail(path, elt) {
+var Tail = (function() {
+    var cursor = null;
+    var waitTime = 0;
+    var cancel = false;
+    var tailPath = null;
+    var tailElt = null;
+    
     var errback = function(xhr, textStatus, errorThrown) {
         if(waitTime === 0) {
             waitTime = 60;
@@ -9,8 +12,9 @@ function tail(path, elt) {
         else {
             waitTime *= 2;
         }
-        setTimeout(function(){ tail(path, elt ); }, waitTime);
+        setTimeout(function(){ tail(path, tailElt ); }, waitTime);
     };
+    
     var callback = function(data) {
         /* If the server hangs up on a long poll, it won't go to errback, so
            we need for force it to do so. */
@@ -20,21 +24,57 @@ function tail(path, elt) {
         waitTime = 0;
         cursor = data.cursor;
         for(var i = 0; i < data.lines.length; ++i) {
-            $('<li></li>').text(data.lines[i]).appendTo(elt);
+            $('<li></li>').text(data.lines[i]).appendTo(tailElt);
         }
-        while($(elt + ' li').length > 100) {
-            $(elt + ' li:first').remove();
+        while($(tailElt + ' li').length > 100) {
+            $(tailElt + ' li:first').remove();
         }
         /* The 9999 here is a hack */
-        $(elt).animate({scrollTop: 9999}, 100);
-        tail(path, elt);
+        $(tailElt).animate({scrollTop: 9999}, 100);
+        if(!cancel) {
+            Tail.tail(tailPath, tailElt);
+        }
     };
-    var queryString = cursor ? {cursor: cursor} : {};
-    $.ajax({
-        url: path,
-        data: queryString,
-        dataType: 'jsonp',
-        success: callback,
-        error: errback
+    
+    return {
+        setup: function(path, elt) {
+            tailPath = path;
+            tailElt = elt;
+        },
+        
+        start: function() {
+            cancel = false;
+            $('#start-tail').hide();
+            $('#stop-tail').show();
+            Tail.tail();
+        },
+        
+        stop: function() {
+            cancel = true;
+            $('#start-tail').show();
+            $('#stop-tail').hide();
+        },
+        
+        tail: function() {
+            var queryString = cursor ? {cursor: cursor} : {};
+            $.ajax({
+                url: tailPath,
+                data: queryString,
+                dataType: 'jsonp',
+                success: callback,
+                error: errback
+            });
+        }
+    };
+})();
+
+$(function() {
+    $('#start-tail').click(function() {
+        Tail.start();
+        return false;
     });
-}
+    $('#stop-tail').click(function() {
+        Tail.stop();
+        return false;
+    });
+});
