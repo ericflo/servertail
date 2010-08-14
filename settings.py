2d9dcc1083cfd0f28d67d678139504e957997bf5
Eric Florenzano <floguy@gmail.com>
2010-08-13 22:32:28 -0700

    Make a management command to run the app with Eventlet

--- settings.py
+++ settings.py
@@ -48,7 +48,7 @@ USE_L10N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, '..', 'media'))
+MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'media'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, handler404, handler500
+from django.conf import settings
 
 from django.contrib import admin
 
@@ -7,3 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'^media/(?P<path>.*)$', 'serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
