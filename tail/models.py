cb0310e5afda8e150ee78844785c9a9f96f9f2b2
Eric Florenzano <floguy@gmail.com>
2010-08-14 10:31:37 -0700

    Remove public key

--- tail/models.py
+++ tail/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import User
 class ServerTail(models.Model):
     hostname = models.TextField()
     port = models.IntegerField(default=22)
-    public_key = models.TextField(blank=True, default='')
-    username = models.TextField(blank=True, default='')
+    username = models.TextField()
     password = models.TextField(blank=True, default='')
     path = models.TextField()
     user = models.ForeignKey(User, null=True, blank=True)
