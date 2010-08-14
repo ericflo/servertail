9f665ff39a171699ef812fbcb12c29a4c475e9e4
Eric Florenzano <floguy@gmail.com>
2010-08-14 10:32:21 -0700

    Make the long polling a bit more robust, and add exponential backoff for failure scenarios.

--- media/js/tail.js
+++ media/js/tail.js
@@ -1,8 +1,23 @@
 var cursor = null;
+var waitTime = 0;
 
 function tail(path, elt) {
-    var queryString = cursor ? {cursor: cursor} : {};
-    $.getJSON(path, queryString, function(data) {
+    var errback = function(xhr, textStatus, errorThrown) {
+        if(waitTime === 0) {
+            waitTime = 60;
+        }
+        else {
+            waitTime *= 2;
+        }
+        setTimeout(function(){ tail(path, elt ); }, waitTime);
+    };
+    var callback = function(data) {
+        /* If the server hangs up on a long poll, it won't go to errback, so
+           we need for force it to do so. */
+        if(!data) {
+            return errback();
+        }
+        waitTime = 0;
         cursor = data.cursor;
         for(var i = 0; i < data.lines.length; ++i) {
             $('<li></li>').text(data.lines[i]).appendTo(elt);
@@ -13,5 +28,13 @@ function tail(path, elt) {
         /* The 9999 here is a hack */
         $(elt).animate({scrollTop: 9999}, 100);
         tail(path, elt);
+    };
+    var queryString = cursor ? {cursor: cursor} : {};
+    $.ajax({
+        url: path,
+        data: queryString,
+        dataType: 'json',
+        success: callback,
+        error: errback
     });
 }
