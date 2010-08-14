2cdc86bfc7635eb2d4dd2d69a994e87090e4d2c4
Eric Florenzano <floguy@gmail.com>
2010-08-14 12:25:26 -0700

    Added date_created field to ServerTail

--- tail/models.py
+++ tail/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -9,6 +11,7 @@ class ServerTail(models.Model):
     password = models.TextField(blank=True, default='')
     path = models.TextField()
     user = models.ForeignKey(User, null=True, blank=True)
+    date_created = models.DateTimeField(default=datetime.datetime.now)
     
     def __unicode__(self):
         return self.hostname
