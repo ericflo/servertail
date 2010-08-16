34d39d97635c5f094d84c2575875f73be9de1f16
Eric Florenzano <floguy@gmail.com>
2010-08-15 19:32:33 -0700

    Added latest front page server_tail to the index page

--- views.py
+++ views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from tail.models import FrontPage
+
 def index(request):
-    context = {}
+    try:
+        server_tail = FrontPage.objects.filter('-date')[0].server_tail
+    except IndexError:
+        server_tail = None
+    context = {
+        'server_tail': server_tail,
+    }
     return render_to_response('index.html', context,
         context_instance=RequestContext(request))
