c2d858b95c7a992d9e513b92a43eefe4bafa1ac4
Eric Florenzano <floguy@gmail.com>
2010-08-15 20:11:52 -0700

    COMMA./bin/deploy ./bin/deploy ./bin/deploy ./bin/deploy ./bin/deploy

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class RegistrationForm(forms.Form):
     username = forms.RegexField(regex=r'^\w+$', max_length=30)
-    password1 = forms.CharField(widget=forms.PasswordInput(render_value=False)
+    password1 = forms.CharField(widget=forms.PasswordInput(render_value=False),
         label='Password')
     password2 = forms.CharField(widget=forms.PasswordInput(render_value=False),
         label='Password (again)')
